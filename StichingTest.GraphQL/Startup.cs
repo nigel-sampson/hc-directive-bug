562dce31bf61f786cba68b330d3939ea11cccb86
Nigel Sampson <nigel.sampson@pushpay.com>
2019-05-27 09:48:16 +1200

    Illustrate directive bug

---
+++ StichingTest.GraphQL/Startup.cs
@@ -0,0 +1,40 @@
+﻿using HotChocolate;
+using HotChocolate.AspNetCore;
+using HotChocolate.Stitching;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+
+namespace StichingTest.GraphQL
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddHttpClient("remote", (sp, client) =>
+            {
+                client.BaseAddress = new Uri("http://localhost:2000");
+            });
+
+            services.AddStitchedSchema(b => b
+                .AddSchemaFromFile("remote", "./Schema.graphql"));
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseGraphQL(new QueryMiddlewareOptions
+            {
+                Path = "/graphql"
+            });
+
+            // force evaulation of schema for fast testing
+            app.ApplicationServices.GetRequiredService<ISchema>();
+        }
+    }
+}
